d29e2d05386b7275375fe0353e3e07d11c596c8a
Jenish <34777295+Muxazuxa@users.noreply.github.com>
2019-08-03 20:41:19 +0600

    Update views.py

--- djangoOrm/orm/views.py
+++ djangoOrm/orm/views.py
@@ -4,11 +4,14 @@ from .models import TaskStatuses, Task
 
 
 def index(request):
-    queryset = Task.objects.filter(created__gte='2019-04-01', created__lt='2019-09-01')\
-        .annotate(Checked=models.Subquery(
+    queryset = Task.objects.filter(
+        created__gte='2019-04-01', created__lt='2019-09-01')\
+        .annotate(
+            Checked=models.Subquery(
             TaskStatuses.objects.annotate(max_datetime=models.Max('created'))
             .filter(task_id=models.OuterRef('id'), status_type=3)
-            .values('max_datetime'), output_field=models.DateTimeField()))\
+            .values('max_datetime'), output_field=models.DateTimeField())
+                 )\
         .values('created', 'description', 'Checked')
     print(queryset.query)
     return HttpResponse(queryset)
